92b2ab147a105c910f264c87baaa4c6e54cd6f02
J-pixel440 <Joshplath1@gmail.com>
2025-01-28 15:38:20 -0600

    Update program_1.py
    By Josh Plath, Jan 28th

--- program_1.py
+++ program_1.py
@@ -1,5 +1,13 @@
 def personal_information():
-    print('...')
+    name="Josh Plath"
+    address="1234 Palace Drive,Palatine, IL 60038"
+    phone=(238)-462-6433
+    college_major="Financial Accountant"
+    print("Name:", name)
+    print("Address:", address)
+    print("Phone:", phone)
+    print("College Major:", college_major)
+
 
 
 # Line which calls the above function.
